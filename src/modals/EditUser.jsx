622c3d8eb8fe0e2c99372fff1f777c7f2dd64e36
WenyiLULU <luwenyi1991@gmail.com>
2022-06-27 12:46:57 +0200

    fix edit user Fixes #15

--- src/modals/EditUser.jsx
+++ src/modals/EditUser.jsx
@@ -1,36 +1,45 @@
 import { useForm } from "@mantine/form";
 import { Group, Button, Modal, TextInput, Checkbox, Textarea} from "@mantine/core";
-import { useNavigate } from "react-router-dom";
 import { SessionContext } from "../context/SessionContext"
 import { useContext } from "react";
+import { useEffect } from "react";
 
 function EditUser({ editModalOpen, setEditModalOpen, user, setUser }) {
     const { apiPutWithToken, apiWithToken } = useContext(SessionContext)
-    const {_id, username, email, country, city, image, owner, sitter, description, experience} = user
-
+    
     const form = useForm({
         initialValues: {
-        username: username,
+        username: "",
         //email: email,
-        country: country,
-        city:city,
-        image:image,
-        owner:owner,
-        sitter:sitter,  
-        description:description, 
-        experience:experience
+        country: "",
+        city:"",
+        image:"",
+        owner:false,
+        sitter:false,  
+        description:"", 
+        experience:""
         },
 
-        validate: {
-        email: (value) =>
-        /^\S+@\S+\.\S+$/.test(value)  ? null : "Please enter a valid e-Mail",
-        }
     });
+
+    useEffect(()=>{
+        if(user?.username){
+            const { username, country, city, image, owner, sitter, description, experience} = user
+            form.setValues({username,
+                country,
+                city,
+                image,
+                owner,
+                sitter,  
+                description, 
+                experience})
+        }
+    }, [user])
 
     const updateUser = async newUserInfo => {
         try {
-        const response = await apiPutWithToken(`user/${_id}`, newUserInfo)
-        const newUser = await apiWithToken(`user/${_id}`)
+        const response = await apiPutWithToken(`user/${user._id}`, newUserInfo)
+        const newUser = await apiWithToken(`user/${user._id}`)
         setUser(newUser)
         if (response.status === 'KO') {
             throw new Error(response.message)
@@ -41,68 +50,50 @@ function EditUser({ editModalOpen, setEditModalOpen, user, setUser }) {
         }
     }
 
-    
     const handleSubmit = (values) => {
+        console.log(values)
         updateUser(values)        
         setEditModalOpen(false)
     }
-
+   
     return (
         <Modal opened={editModalOpen} onClose={() => setEditModalOpen(false)} title='EditUser'>
         <form onSubmit={form.onSubmit(handleSubmit)}>
             <TextInput
                 label="Username"
-                placeholder={username}
-                value={username}
-                {...form.getInputProps("username")}
+                {...form.getInputProps("username")} 
             />
-
-            {/*<TextInput 
-                label="Email"
-                placeholder={email}
-                value={email}
-                {...form.getInputProps("email")}
-            />*/}
 
             <TextInput
                 label="Country"
-                placeholder={country}
-                value={country}
                 {...form.getInputProps("country")}
             />
 
             <TextInput
-                label="City"
-                placeholder={city}
-                value={city}
+                label="City" 
                 {...form.getInputProps("city")}
             />
             <Checkbox
                 label="I'm a pets owner"
                 color="lime"
-                checked={owner}
                 {...form.getInputProps("owner")}
              />
              <Checkbox
                 label="I want to be a sitter of pets"
-                checked={sitter}
                 color="lime"
                 {...form.getInputProps("sitter")}
              />
             <Textarea
                 label="About you"
-                placeholder={description}
                 {...form.getInputProps("description")}
             />
             <Textarea
-                label="Experience of take care pets"
-                placeholder={experience}
+                label="Experience of take care pets"                
                 {...form.getInputProps("experience")}
             />
-            
 
             <Group position="right" mt="md">
-            <Button type="submit">Save</Button>
+                <Button type="submit">Save</Button>
             </Group>
         </form>
         </Modal>
