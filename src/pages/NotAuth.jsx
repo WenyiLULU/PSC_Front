f3888a99a21f1bf6a25f1b7793dfc7571666ef36
WenyiLULU <luwenyi1991@gmail.com>
2022-06-30 09:38:06 +0200

    style setting Fixes #29 - not auth page

--- src/pages/NotAuth.jsx
+++ src/pages/NotAuth.jsx
@@ -1,6 +1,21 @@
+import raccoon from "../assets/raccoon.png"
+import {Image, Center} from "@mantine/core"
 const NotAuth = () => {
     return ( 
+        <Center style={{display:"flex", flexDirection:"column", fontColor:"#302e36"}}>
         <h1> Please login to see more infomations ^_^</h1>
+        <Image 
+            src={raccoon} 
+            alt="oups"
+            sx={()=>({
+                width: "400px",
+                '@media (max-width: 400px)':{
+                    width:"300px"
+                }
+            })} 
+            />
+        </Center>
+        
      );
 }
  
