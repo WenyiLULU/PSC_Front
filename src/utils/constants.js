97644dce292d5a7953067db101c9266de1d7e8ff
WenyiLULU <luwenyi1991@gmail.com>
2022-06-30 22:46:02 +0200

    Chang api url to herokuapp

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-export const BASE_API_URL = 'http://localhost:5005/api'
-export const AUTH_API_URL = 'http://localhost:5005/auth'
+export const BASE_API_URL = 'https://pet-social-club.herokuapp.com/api'
+export const AUTH_API_URL = 'https://pet-social-club.herokuapp.com/auth'
